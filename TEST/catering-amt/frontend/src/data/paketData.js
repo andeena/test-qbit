5eedcc552afbb7b536891522b023795de77031ba
andeena <andinabks@gmail.com>
2025-07-25 17:47:19 +0700

    add image

--- TEST/catering-amt/frontend/src/data/paketData.js
+++ TEST/catering-amt/frontend/src/data/paketData.js
@@ -3,30 +3,30 @@ export const dataPaket = {
     namaKategori: "Paket Tumpeng",
     deskripsi: "Pusat perhatian di setiap acara, tumpeng kami dibuat dengan bahan terbaik dan presentasi yang memukau.",
     subPaket: [
-      { nama: "Tumpeng Mini (5 Porsi)", harga: "Rp 250.000", img: "https://i.imgur.com/gK2Bb4L.jpg", isi: ["Nasi Kuning", "Ayam Goreng", "Perkedel", "Kering Tempe", "Sambal"] },
-      { nama: "Tumpeng Silver (20 Porsi)", harga: "Rp 750.000", img: "https://i.imgur.com/8Qk3v2j.jpg", isi: ["Nasi Kuning", "Ayam Bakar", "Perkedel", "Kering Tempe", "Sambal Goreng Ati", "Urap"] },
-      { nama: "Tumpeng Gold (30 Porsi)", harga: "Rp 1.100.000", img: "https://i.imgur.com/G5g2w6A.jpg", isi: ["Nasi Kuning", "Ayam Bakar", "Empal Suwir", "Kering Tempe", "Sambal Goreng Ati", "Urap", "Telur Pindang"] },
-      { nama: "Tumpeng Platinum (50 Porsi)", harga: "Rp 1.850.000", img: "https://i.imgur.com/sS5i2zO.jpg", isi: ["Nasi Kuning", "Ayam Bakar", "Empal Suwir", "Udang Goreng", "Kering Tempe", "Sambal Goreng Ati", "Urap", "Telur Pindang", "Sayur Asem"] }
+      { nama: "Tumpeng Mini (1 Porsi)", harga: "Rp 50.000", img: "https://rajatumpeng.com/wp-content/uploads/2021/11/tumpeng-mini-30k-1.png", isi: ["Nasi Kuning", "Ayam Goreng", "Perkedel", "Mie Kuning", "Kering Tempe", "Sambal Goreng Ati", "Telur", "Sambal"] },
+      { nama: "Tumpeng Silver (5 Porsi)", harga: "Rp 180.000", img: "https://down-id.img.susercontent.com/file/id-11134207-7qul5-lhcy8ux0z41vde", isi: ["Nasi Kuning", "Ayam Bakar", "Perkedel", "Kering Tempe", "Sambal Goreng Ati", "Mie Kuning"] },
+      { nama: "Tumpeng Gold (20 Porsi)", harga: "Rp 400.000", img: "https://images.tokopedia.net/img/cache/500-square/VqbcmM/2021/1/5/2ffe59bb-c4fa-4426-9e5c-1cfe356dc4aa.jpg?ect=4g", isi: ["Nasi Kuning", "Ayam Bakar", "Sambal Goreng Ati", "Urap", "Telur Bali", "Mie Kuning"] },
+      { nama: "Tumpeng Platinum (50 Porsi)", harga: "Rp 900.000", img: "https://img.mbizmarket.co.id/products/thumbs/800x800/2023/01/24/99f741e26eef28f6c0049b6060f9cdf4.jpg", isi: ["Nasi Kuning", "Ayam Bakar", "Empal Suwir", "Udang Goreng", "Kering Tempe", "Sambal Goreng Ati", "Urap", "Perkedel"] }
     ]
   },
   "nasi-box": {
     namaKategori: "Paket Nasi Box",
     deskripsi: "Praktis, higienis, dan lezat. Solusi sempurna untuk konsumsi rapat, gathering, atau tour.",
     subPaket: [
-      { nama: "Box Hemat", harga: "Rp 25.000", img: "https://i.imgur.com/t322dnE.jpg", isi: ["Nasi Putih", "Ayam Goreng", "Tahu & Tempe Bacem", "Sambal Lalap"] },
-      { nama: "Box Komplit", harga: "Rp 35.000", img: "https://i.imgur.com/k9Y2T2a.jpg", isi: ["Nasi Putih", "Ayam Bakar Madu", "Tumis Buncis", "Kerupuk", "Buah"] },
-      { nama: "Box Spesial", harga: "Rp 45.000", img: "https://i.imgur.com/L1uA0d9.jpg", isi: ["Nasi Putih", "Rendang Daging", "Sayur Nangka", "Sambal Ijo", "Perkedel"] },
-      { nama: "Box Premium", harga: "Rp 60.000", img: "https://i.imgur.com/yv9vVlO.jpg", isi: ["Nasi Daun Jeruk", "Gurame Asam Manis", "Capcay", "Udang Tempura", "Puding"] }
+      { nama: "Box Hemat", harga: "Rp 15.000", img: "https://cdn.manisdansedap.com/img_detail/16563794312714056.jpg", isi: ["Nasi Putih", "Ayam Bakar", "Tahu & Tempe", "Sambal Lalap"] },
+      { nama: "Box Komplit", harga: "Rp 25.000", img: "https://gratisongkir-storage.com/products/900x900/FWwhA9A615Jh.jpg", isi: ["Nasi Putih", "Ayam Bakar Madu", "Tempe Kering", "Telur Kecap", "Sambal Goreng Kentang", "Kerupuk", "Buah"] },
+      { nama: "Box Spesial", harga: "Rp 32.000", img: "https://img.mbizmarket.co.id/products/thumbs/800x800/2021/11/17/bfd88e58eaffef498aa7fa7a9479c6e3.jpg", isi: ["Nasi Putih", "Rendang Daging", "Cap Cai", "Acar", "Perkedel", "Kentang Mustofa", "Telur Kare"] },
+      { nama: "Box Premium", harga: "Rp 45.000", img: "https://img.mbizmarket.co.id/products/thumbs/800x800/2023/02/09/efc4c3ba7c298f603162590825f9942a.jpg", isi: ["Nasi Daun Jeruk", "Gurame Bakar", "Capcay", "Udang Tempura", "Puding"] }
     ]
   },
   prasmanan: {
     namaKategori: "Layanan Prasmanan",
     deskripsi: "Biarkan kami yang mengurus semuanya. Layanan buffet profesional untuk acara pernikahan, kantor, dan lainnya.",
     subPaket: [
-      { nama: "Prasmanan Bronze", harga: "Rp 75.000 /pax", img: "https://i.imgur.com/xTz57v5.jpg", isi: ["Nasi Putih", "Sop Ayam Jamur", "Ayam Goreng", "Kakap Asam Manis", "Air Mineral"] },
-      { nama: "Prasmanan Silver", harga: "Rp 95.000 /pax", img: "https://i.imgur.com/yv9vVlO.jpg", isi: ["Nasi Putih", "Nasi Goreng", "Sop Kimlo", "Ayam Saus Inggris", "Daging Lada Hitam", "Buah & Puding"] },
-      { nama: "Prasmanan Gold", harga: "Rp 125.000 /pax", img: "https://i.imgur.com/t322dnE.jpg", isi: ["Nasi Putih", "Nasi Goreng Seafood", "Sop Buntut", "Ayam Kodok", "Gurame Bakar", "Sate Ayam", "Salad & Aneka Kue"] },
-      { nama: "Prasmanan Platinum", harga: "Hubungi Kami", img: "https://i.imgur.com/G5g2w6A.jpg", isi: ["Menu custom sesuai permintaan", "Termasuk dekorasi meja buffet", "Pramusaji profesional", "Stall makanan (Gubukan)"] }
+      { nama: "Prasmanan Bronze", harga: "Rp 75.000 /pax", img: "https://img.mbizmarket.co.id/products/thumbs/800x800/2022/10/10/904a7ca9bfcc9a4e8697605ce08f814f.jpg", isi: ["Nasi Putih", "Sop Ayam Jamur", "Ayam Goreng", "Kakap Asam Manis", "Air Mineral"] },
+      { nama: "Prasmanan Silver", harga: "Rp 95.000 /pax", img: "https://www.sajianbunda.com/wp-content/uploads/2020/09/Tips-Memilih-Catering-Prasmanan-di-Pernikahan-1024x681.jpg", isi: ["Nasi Putih", "Nasi Goreng", "Sop Kimlo", "Ayam Saus Inggris", "Daging Lada Hitam", "Buah & Puding"] },
+      { nama: "Prasmanan Gold", harga: "Rp 125.000 /pax", img: "https://img.mbizmarket.co.id/products/thumbs/800x800/2022/12/27/dffe0c2f7b4a4a645170f333020f3e37.jpg", isi: ["Nasi Putih", "Nasi Goreng Seafood", "Sop Buntut", "Ayam Kodok", "Gurame Bakar", "Sate Ayam", "Salad & Aneka Kue"] },
+      { nama: "Prasmanan Platinum", harga: "Hubungi Kami", img: "https://www.weddinghnr.com/wp-content/uploads/2015/04/pondokan.jpg", isi: ["Menu custom sesuai permintaan", "Termasuk dekorasi meja buffet", "Pramusaji profesional", "Stall makanan (Gubukan)"] }
     ]
   }
 };
