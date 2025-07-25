26acf1e4dfdf23c45aed3e8bae31a9852b50b56d
andeena <andinabks@gmail.com>
2025-07-25 18:55:21 +0700

    add instagram

--- TEST/catering-amt/frontend/src/components/Footer.jsx
+++ TEST/catering-amt/frontend/src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { InstagramEmbed } from 'react-social-media-embed';
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Catering AMT. All Rights Reserved.</p>
+      <p>&copy; 2025 Omah Catering "AMT". All Rights Reserved.</p>
+      <p>Follow us on:</p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <InstagramEmbed url="https://www.instagram.com/omahcatering_amt" width={328} />
+      </div>
     </footer>
   );
 }
--- TEST/catering-amt/frontend/src/components/Header.jsx
+++ TEST/catering-amt/frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
       <div className="container">
-        <h1 className="logo">Catering AMT</h1>
+        <h1 className="logo">Omah Catering "AMT"</h1>
         <nav>
           <a href="#menu">Menu</a>
           <a href="#galeri">Galeri</a>
